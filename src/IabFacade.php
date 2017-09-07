f4ea77e3385f2b0619f3529fd8b2e4251d043719
Elegi Sandi <elegi@crowdmetric.com>
2017-09-07 17:03:32 +0800

    added method doc on iabfacade class

--- src/IabFacade.php
+++ src/IabFacade.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class IabFacade
  * @package elegisandi\IABBotDetect
+ *
+ * @method static Validator setUserAgent($user_agent = null)
+ * @method static Validator setCredentials(array $credentials)
+ * @method static Validator isValidBrowser($user_agent = null)
+ * @method static Validator isBot($user_agent = null, $white_list = true)
+ * @method static Validator initialize($overwrite = false)
  */
 class IabFacade extends Facade
 {

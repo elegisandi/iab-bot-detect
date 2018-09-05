bc493d4540bbe9dd41dbcf9cbecada2e115a0e48
Elegi Sandi <elegi@crowdmetric.com>
2018-09-05 15:33:12 +0800

    fix facade method docs

--- src/IabFacade.php
+++ src/IabFacade.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Facade;
  * Class IabFacade
  * @package elegisandi\IABBotDetect
  *
- * @method static Validator setUserAgent($user_agent = null)
- * @method static Validator setCredentials(array $credentials)
- * @method static Validator isValidBrowser($user_agent = null)
- * @method static Validator isBot($user_agent = null, $white_list = true)
- * @method static Validator initialize($overwrite = false)
+ * @method static void setUserAgent($user_agent = null)
+ * @method static void setCredentials(array $credentials)
+ * @method static bool isValidBrowser($user_agent = null)
+ * @method static bool isBot($user_agent = null, $white_list = true)
+ * @method static void initialize($overwrite = false)
  */
 class IabFacade extends Facade
 {
